06b4021f6c9c5fbbeaf880c9991f66055940a164
ValentinLuz <78982609+ValentinLuz@users.noreply.github.com>
2021-02-18 14:18:17 +0100

    Update org_decision students.py

--- org_decision students.py
+++ org_decision students.py
@@ -56,15 +56,43 @@ def choose_individual(value_p, per_e_mid):
 
 def choose_delegate(value_p, per_e_low, per_e_mid, per_e_high, type_p,
                     e_low, e_mid, e_high):
-    performance = 0
+    if type_p > (e_high+e_mid)/2:
+        if per_e_high > 0:
+            performance = value_p
+        else:
+            performance = 0
+    elif type_p < (e_mid+e_low)/2:
+        if per_e_low > 0:
+            performance = value_p
+        else:
+            performance = 0
+    else: 
+        if per_e_mid > 0:
+            performance = value_p
+        else:
+            performance = 0
     return performance
 
 def choose_voting(value_p, per_e_low, per_e_mid, per_e_high):
-    performance = 0
+    summe = 0
+    if per_e_low > 0:
+        summe += 1
+    if per_e_mid > 0:
+        summe += 1
+    if per_e_high > 0:
+        summe += 1
+    if summe >= 2:
+        performance = value_p
+    else:
+        performance = 0
     return performance
 
 def choose_average(value_p, per_e_low, per_e_mid, per_e_high):
-    performance = 0
+    avg = (per_e_low + per_e_mid + per_e_high)/3
+    if avg > 0:
+        performance = value_p
+    else:
+        performance = 0
     return performance
 
 
